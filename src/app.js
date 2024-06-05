ef5f56bf5c31cd285f8078b4a2689bafda2ce852
amarkos27 <armarkos27@gmail.com>
2024-06-05 13:35:33 -0400

    Create rough sidebar animation

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 import './style.css';
 import 'normalize.css';
+
+const button = document.querySelector('.test');
+const sidebar = document.querySelector('.sidebar');
+const items = document.querySelector('.items');
+
+button.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebar-swipe');
+  button.classList.toggle('button-pushed');
+});
