cf9ea97a813d2efa31f8a5bf3119eb52ca3b7fa4
amarkos27 <armarkos27@gmail.com>
2024-06-05 12:03:38 -0400

    Basic page layout

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    app: './app.js',
+    app: './src/app.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
