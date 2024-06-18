356f10126f8e8550b0a441c3db0cdc1351473b23
amarkos27 <armarkos27@gmail.com>
2024-06-18 14:35:05 -0400

    Adjust margins for even spacing on sidebar disappear

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import './style.css';
 
 const button = document.querySelector('.sidebar-btn');
 const sidebar_wrapper = document.querySelector('.sidebar-wrapper');
-const items = document.querySelector('.items');
+const task_display = document.querySelector('.task-display');
 
 button.addEventListener('click', () => {
   sidebar_wrapper.classList.toggle('sidebar-swipe-out');
+  task_display.classList.toggle('full-screen');
   button.classList.toggle('btn-out');
 });
