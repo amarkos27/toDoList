c4257e70c1253846f4b954594bc4353a5a89701c
amarkos27 <armarkos27@gmail.com>
2024-06-24 15:39:51 -0400

    Rough draft of window_resize display func

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import 'normalize.css';
 import './style.css';
-import { toggle_sidebar } from './display.js';
+import { display_init } from './display.js';
 
 function init() {
-  const sidebar_btn = document.querySelector('.sidebar-btn');
-  sidebar_btn.addEventListener('click', toggle_sidebar);
+  display_init();
 }
 
 init();
--- src/display.js
+++ src/display.js
@@ -1,5 +1,26 @@
-export function toggle_sidebar() {
+function toggle_sidebar(sidebar) {
+  sidebar.classList.toggle('sidebar-closed');
+}
+
+function window_resize(sidebar) {
+  const shouldCloseSidebar = window.innerWidth < 1100;
+  const sidebarClosed = sidebar.classList.contains('sidebar-closed');
+
+  if (shouldCloseSidebar && !sidebarClosed) {
+    sidebar.classList.add('sidebar-closed');
+  } else if (!shouldCloseSidebar && sidebarClosed) {
+    sidebar.classList.remove('sidebar-closed');
+  }
+}
+
+export function display_init() {
   const sidebar_wrapper = document.querySelector('.sidebar-wrapper');
-  sidebar_wrapper.classList.toggle('sidebar-swipe-out');
-  this.classList.toggle('btn-out');
+  const sidebar_btn = document.querySelector('.sidebar-btn');
+  sidebar_btn.addEventListener('click', () => {
+    toggle_sidebar(sidebar_wrapper);
+  });
+
+  window.addEventListener('resize', () => {
+    window_resize(sidebar_wrapper);
+  });
 }
