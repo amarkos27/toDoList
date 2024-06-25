5d95fd4b5e96b0014cc3f8de171256721ee86b8c
amarkos27 <armarkos27@gmail.com>
2024-06-25 11:44:06 -0400

    Create proper sidebar behavior on window resize

--- src/display.js
+++ src/display.js
@@ -1,14 +1,23 @@
 function toggle_sidebar(sidebar) {
+  const alreadyClosed = sidebar.classList.contains('sidebar-closed');
   sidebar.classList.toggle('sidebar-closed');
+
+  if (!alreadyClosed) {
+    sidebar.classList.add('toggled-off');
+  } else {
+    sidebar.classList.remove('toggled-off');
+  }
 }
 
-function window_resize(sidebar) {
-  const shouldCloseSidebar = window.innerWidth < 1100;
+function window_resize(sidebar, previousWidth) {
+  const windowSmall = window.innerWidth < 1100;
+  const toggledOff = sidebar.classList.contains('toggled-off');
   const sidebarClosed = sidebar.classList.contains('sidebar-closed');
+  const shrinking = window.innerWidth < previousWidth;
 
-  if (shouldCloseSidebar && !sidebarClosed) {
+  if (windowSmall && shrinking && !sidebarClosed) {
     sidebar.classList.add('sidebar-closed');
-  } else if (!shouldCloseSidebar && sidebarClosed) {
+  } else if (!windowSmall && !toggledOff && sidebarClosed) {
     sidebar.classList.remove('sidebar-closed');
   }
 }
@@ -16,11 +25,14 @@ function window_resize(sidebar) {
 export function display_init() {
   const sidebar_wrapper = document.querySelector('.sidebar-wrapper');
   const sidebar_btn = document.querySelector('.sidebar-btn');
+  let previousWidth = window.innerWidth;
+
   sidebar_btn.addEventListener('click', () => {
     toggle_sidebar(sidebar_wrapper);
   });
 
   window.addEventListener('resize', () => {
-    window_resize(sidebar_wrapper);
+    window_resize(sidebar_wrapper, previousWidth);
+    previousWidth = window.innerWidth;
   });
 }
