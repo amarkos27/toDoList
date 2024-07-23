8a67de8336b7b264b1af6f12c0612c7c7f794c0d
amarkos27 <armarkos27@gmail.com>
2024-07-23 18:29:35 -0400

    Add open and closing animations to new-task modal

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import 'normalize.css';
 import './style.css';
-import { display_init, open_modal } from './display.js';
+import { display_init, new_task_form } from './display.js';
 
 function init() {
   display_init();
 
   const new_task = document.querySelector('.add-task');
   new_task.addEventListener('click', () => {
-    open_modal();
+    new_task_form();
   });
 }
 
