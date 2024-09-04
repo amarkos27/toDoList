a50f8c504106697e811e4c132b148f0dc6c92931
amarkos27 <armarkos27@gmail.com>
2024-09-04 13:44:00 -0400

    Add action buttons to project buttons

--- src/project.js
+++ src/project.js
@@ -6,6 +6,20 @@ class Project {
     this.display.type = 'button';
     this.display.classList.add('option');
     this.display.textContent = projectName;
+
+    this.actions = document.createElement('div');
+    this.actions.classList.add('project-actions', 'actions');
+
+    this.editBtn = document.createElement('div');
+    this.editBtn.classList.add('edit');
+
+    this.deleteBtn = document.createElement('div');
+    this.deleteBtn.classList.add('delete');
+
+    this.actions.appendChild(this.editBtn);
+    this.actions.appendChild(this.deleteBtn);
+
+    this.display.appendChild(this.actions);
   }
 }
 
